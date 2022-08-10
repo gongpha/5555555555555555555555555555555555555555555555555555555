b776ab4e8705db40dac0ee1defeceabab45fd12c
Kongfa Waroros <gongpha@hotmail.com>
2022-08-11 00:11:05 +0700

    55555555555555555555555555555555555
    55555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555

---
+++ 5555555555555555555555555555555555555555555555555555555.gd
@@ -0,0 +1,10 @@
+class _555 :
+	func _555() -> int :
+		while Time.get_ticks_msec() % 555 != 55 :
+			print("5555555555555555555555555555")
+		return 5555555555555
+			
+func _process(d : float) :
+	var _55555 := _555.new()
+	var _5555 := _55555._555()
+	push_error("5555 : 5555 -> " + str(_5555))
